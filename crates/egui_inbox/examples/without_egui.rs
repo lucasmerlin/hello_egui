092e9b134d401f4ffb157624d714c356d718164d
Lucas Meurer <lucasmeurer96@gmail.com>
2024-01-27 00:38:56 +0100

    Make inbox work without egui

---
+++ crates/egui_inbox/examples/without_egui.rs
@@ -0,0 +1,57 @@
+use egui_inbox::{AsRequestRepaint, RequestRepaintContext, UiInbox};
+
+pub struct MyApplicationState {
+    state: Option<String>,
+    inbox: UiInbox<String>,
+    repaint_rx: std::sync::mpsc::Receiver<()>,
+    repaint_tx: std::sync::mpsc::Sender<()>,
+}
+
+impl AsRequestRepaint for MyApplicationState {
+    fn as_request_repaint(&self) -> RequestRepaintContext {
+        let repaint_tx = self.repaint_tx.clone();
+        RequestRepaintContext::from_callback(move || {
+            repaint_tx.send(()).unwrap();
+        })
+    }
+}
+
+impl Default for MyApplicationState {
+    fn default() -> Self {
+        let (repaint_tx, repaint_rx) = std::sync::mpsc::channel();
+        Self {
+            state: None,
+            inbox: UiInbox::new(),
+            repaint_rx,
+            repaint_tx,
+        }
+    }
+}
+
+impl MyApplicationState {
+    pub fn run(mut self) {
+        let sender = self.inbox.sender();
+        std::thread::spawn(move || {
+            let mut count = 0;
+            loop {
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                count += 1;
+                sender.send(format!("Count: {}", count)).ok();
+            }
+        });
+
+        loop {
+            self.inbox.read(&self).for_each(|msg| {
+                self.state = Some(msg);
+            });
+
+            println!("State: {:?}", self.state);
+
+            self.repaint_rx.recv().unwrap();
+        }
+    }
+}
+
+fn main() {
+    MyApplicationState::default().run();
+}
