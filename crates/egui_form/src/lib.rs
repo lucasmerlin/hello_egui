fb4118fa88a29d0f68bd1ab6459ccf99d6119f52
Lucas Meurer <lucasmeurer96@gmail.com>
2024-05-05 00:25:26 +0200

    Move form_field

--- crates/egui_form/src/lib.rs
+++ crates/egui_form/src/lib.rs
@@ -2,9 +2,6 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
-use egui::{Response, RichText, Widget};
-use std::borrow::Cow;
-
 mod form;
 
 /// To use [garde] with egui_form, you need to create a [garde::GardeReport] and pass it to the [Form] instance.
@@ -67,6 +64,7 @@ mod form;
 pub mod garde;
 mod validation_report;
 
+mod form_field;
 /// To use [validator] with egui_form, you need to create a [validator::ValidatorReport] and pass it to the [Form] instance.
 ///
 /// Then, when you create a [FormField], you pass a slice of [validator::PathItem]s.
@@ -147,110 +145,6 @@ mod validation_report;
 #[cfg(feature = "validator_validator")]
 pub mod validator;
 
-use crate::form::FormFieldState;
 pub use form::Form;
+pub use form_field::*;
 pub use validation_report::EguiValidationReport;
-
-/// A form field that can be validated.
-/// Will color the field red (using the color from [egui::style::Visuals]::error_fg_color) if there is an error.
-/// Will show the error message below the field if the field is blurred and there is an error.
-pub struct FormField<'a, 'f, Errors: EguiValidationReport> {
-    error: Option<Cow<'static, str>>,
-    label: Option<Cow<'a, str>>,
-    form: Option<&'f mut Form<Errors>>,
-}
-
-impl<'a, 'f, Errors: EguiValidationReport> FormField<'a, 'f, Errors> {
-    /// Create a new FormField.
-    /// Pass a [form::Form] and a reference to the field you want to validate.
-    /// If you use [garde], just pass the field name / path as a string.
-    /// If you use [validator], pass a field reference using the [crate::field_path] macro.
-    pub fn new<'c>(form: &'f mut form::Form<Errors>, field: Errors::FieldPath<'c>) -> Self {
-        let error = form
-            .validation_results
-            .iter()
-            .find_map(|errors| errors.get_field_error(field));
-
-        FormField {
-            error,
-            label: None,
-            form: Some(form),
-        }
-    }
-
-    /// Optionally set a label for the field.
-    pub fn label(mut self, label: impl Into<Cow<'a, str>>) -> Self {
-        self.label = Some(label.into());
-        self
-    }
-
-    /// Render the field.
-    pub fn ui(self, ui: &mut egui::Ui, content: impl Widget) -> Response {
-        let error = self.error;
-
-        ui.vertical(|ui| {
-            let id = ui.auto_id_with("form_field");
-            let blurred = ui.memory_mut(|mem| *mem.data.get_temp_mut_or(id, false));
-
-            let error_color = ui.style().visuals.error_fg_color;
-
-            let show_error = error.is_some() && blurred;
-
-            if show_error {
-                let widgets = &mut ui.style_mut().visuals.widgets;
-                widgets.inactive.bg_stroke.color = error_color;
-                widgets.inactive.bg_stroke.width = 1.0;
-                widgets.active.bg_stroke.color = error_color;
-                widgets.active.bg_stroke.width = 1.0;
-                widgets.hovered.bg_stroke.color = error_color;
-                widgets.hovered.bg_stroke.width = 1.0;
-                widgets.open.bg_stroke.color = error_color;
-                widgets.open.bg_stroke.width = 1.0;
-            }
-
-            if let Some(label) = self.label {
-                let mut rich_text = RichText::new(label.as_ref());
-                if show_error {
-                    rich_text = rich_text.color(error_color);
-                }
-                ui.label(rich_text);
-            }
-
-            let response = content.ui(ui);
-
-            if response.lost_focus() {
-                ui.memory_mut(|mem| {
-                    mem.data.insert_temp(id, true);
-                });
-            }
-
-            if let Some(form) = self.form {
-                if let Some(error) = &error {
-                    form.controls.push(FormFieldState {
-                        state_id: id,
-                        widget_id: response.id,
-                        errors: vec![error.to_string()],
-                    });
-                } else {
-                    form.controls.push(FormFieldState {
-                        state_id: id,
-                        widget_id: response.id,
-                        errors: vec![],
-                    });
-                };
-            }
-
-            ui.add_visible(
-                show_error,
-                egui::Label::new(
-                    RichText::new(error.as_deref().unwrap_or(""))
-                        .color(error_color)
-                        .small(),
-                ),
-            );
-
-            response
-        })
-        .inner
-    }
-}
