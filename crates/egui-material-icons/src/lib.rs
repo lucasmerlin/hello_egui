956341fab09688ae9cb7f8fff8b27e303b67e28a
Nicolas <bircni@icloud.com>
2024-10-01 14:16:04 +0200

    add egui-material-icons & move workspace dependencies

---
+++ crates/egui-material-icons/src/lib.rs
@@ -0,0 +1,43 @@
+use egui::{
+    Button, FontData, FontDefinitions, FontFamily, Frame, Margin, Response, RichText, Widget,
+};
+
+pub mod icons {
+    material_icons_proc_macro::code_points!();
+}
+
+pub const FONT_DATA: &[u8] = include_bytes!("../MaterialSymbolsRounded_Filled-Regular.ttf");
+
+pub fn initialize(ctx: &egui::Context) {
+    let mut fonts = FontDefinitions::default();
+    let mut data = FontData::from_static(FONT_DATA);
+    data.tweak.y_offset_factor = 0.05;
+    fonts.font_data.insert("material-icons".to_string(), data);
+    fonts
+        .families
+        .get_mut(&FontFamily::Proportional)
+        .unwrap()
+        .push("material-icons".to_string());
+
+    ctx.set_fonts(fonts);
+}
+
+pub fn icon_button(ui: &mut egui::Ui, icon: &str) -> Response {
+    Frame::none()
+        .inner_margin(Margin {
+            left: 0.0,
+            right: 0.0,
+            top: 0.0,
+            bottom: 0.0,
+        })
+        .show(ui, |ui| {
+            Button::new(RichText::new(icon).size(18.0))
+                .frame(false)
+                .ui(ui)
+        })
+        .inner
+}
+
+pub fn icon_text(icon: &str) -> RichText {
+    RichText::new(icon)
+}
---
+++ crates/material-icons-proc-macro/src/lib.rs
@@ -0,0 +1,35 @@
+use proc_macro::TokenStream;
+use std::collections::HashSet;
+
+#[proc_macro]
+pub fn code_points(_item: TokenStream) -> TokenStream {
+    let codepoints = include_str!("../MaterialSymbolsRounded-Regular.codepoints");
+
+    let mut names = HashSet::new();
+
+    let code: String = codepoints
+        .split("\n")
+        .filter_map(|point| {
+            let split_point: Vec<&str> = point.split(" ").collect();
+
+            if split_point.len() > 1 {
+                let name = split_point[0].to_uppercase();
+                let addr = split_point[1];
+
+                if !names.contains(&name) {
+                    let token = Some(format!(
+                        "pub const ICON_{name}: &str = \"\\u{{{addr}}}\";\n"
+                    ));
+                    names.insert(name);
+                    token
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    code.parse().unwrap()
+}
