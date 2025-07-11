9cbf165fda102a5e2f2fece5f0b89812fb2e2750
Lucas Meurer <lucasmeurer96@gmail.com>
2025-07-11 11:41:02 +0200

    Fix current layer transform not applied to dragged item (#69)

--- crates/egui_dnd/examples/simple.rs
+++ crates/egui_dnd/examples/simple.rs
@@ -1,25 +1,29 @@
 use eframe::{egui, NativeOptions};
-use egui::CentralPanel;
+use egui::{CentralPanel, Rect, Scene};
 use egui_dnd::dnd;
 
 pub fn main() -> eframe::Result<()> {
     let mut items = vec!["alfred", "bernhard", "christian"];
+
+    let mut rect = Rect::ZERO;
 
     eframe::run_simple_native(
         "DnD Simple Example",
         NativeOptions::default(),
         move |ctx, _frame| {
             CentralPanel::default().show(ctx, |ui| {
-                dnd(ui, "dnd_example").show_vec(&mut items, |ui, item, handle, state| {
-                    ui.horizontal(|ui| {
-                        handle.ui(ui, |ui| {
-                            if state.dragged {
-                                ui.label("dragging");
-                            } else {
-                                ui.label("drag");
-                            }
+                Scene::new().show(ui, &mut rect, |ui| {
+                    dnd(ui, "dnd_example").show_vec(&mut items, |ui, item, handle, state| {
+                        ui.horizontal(|ui| {
+                            handle.ui(ui, |ui| {
+                                if state.dragged {
+                                    ui.label("dragging");
+                                } else {
+                                    ui.label("drag");
+                                }
+                            });
+                            ui.label(*item);
                         });
-                        ui.label(*item);
                     });
                 });
             });
